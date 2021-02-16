026b325d9bd28ff603b52b42e19ce6b1ff1a87f0
Spanarchian <spanarchian@gmail.com>
2021-02-16 08:57:54 +0000

    REFACTORED: to remove Codacy errors

--- datamgt/python/graph.py
+++ datamgt/python/graph.py
@@ -1,4 +1,3 @@
-import pandas as pd
 from py2neo import Graph
 
 graph = Graph("bolt://localhost:7687")
@@ -12,17 +11,15 @@ queryStr1 = "MATCH (a:CELL)-[:CURRENT_STATE]->()  RETURN a.cell_ref"
 def executeQry_df(queryStr):
     dat = graph.run(queryStr).to_data_frame()
     print(f"dat = {dat[0]}")
-    
-tbl_qry = "UNWIND range(1, 3) AS n RETURN n, n * n as n_sq"   
-    
+
+tbl_qry = "UNWIND range(1, 3) AS n RETURN n, n * n as n_sq"
+
 def executeQry_tbl(queryStr):
     tbl = graph.run(queryStr).to_table()
     print(f"tbl = {tbl}")
-      
-    
+
+
 executeQry_df(queryStr)
 executeQry_df(queryStr1)
 
 executeQry_tbl(tbl_qry)
-
-
